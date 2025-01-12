99ab8ecd445cb14ea9a44fb672805ae86ff810c1
Anandu-Vijayan <ananduvijayan8@gmail.com>
2025-01-12 20:59:29 +0530

    Integer to Roman updated

--- Integer to Roman.js
+++ Integer to Roman.js
@@ -65,28 +65,19 @@
 
 
 var intToRoman = function(num) {
-    const romanNum = {
-   V̆̈: 5000,
-   M: 1000,
-   CM: 900,
-   D: 500,
-   CD: 400,
-   C: 100,
-   XC: 90,
-   L: 50,
-   XL: 40,
-   X: 10,
-   IX: 9,
-   V: 5,
-   IV: 4,
-   I: 1,
- }
- var str = ''
-  for (var i of Object.keys(romanNum)) {
-   var q = Math.floor(num / romanNum[i])
-   num -= q * romanNum[i]
-   str += i.repeat(q)
- }
- return str
+  const valueSymbols = [
+    [1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'],
+    [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'],
+    [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']
+  ];
+  let res = '';
 
+  for (const [value, symbol] of valueSymbols) {
+    if (num === 0) break;
+    const count = Math.floor(num / value);
+    res += symbol.repeat(count);
+    num -= count * value;
+  }
+
+  return res;    
 };
