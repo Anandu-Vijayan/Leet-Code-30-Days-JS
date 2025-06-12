4ee5fea793d34f94e057179736b827bd41f89699
Anandu-Vijayan <ananduvijayan8@gmail.com>
2025-06-12 13:22:22 +0530

    Find K Pairs with Smallest Sums

--- Super Pow.js
+++ Super Pow.js
@@ -28,3 +28,23 @@
 // 1 <= b.length <= 2000
 // 0 <= b[i] <= 9
 // b does not contain leading zeros.
+
+
+var superPow = function(a, b) {
+	const MOD = 1337;
+	const pow = (num, n) => {
+		let result = 1;
+		for (let index = 0; index < n; index++) {
+			result = result * num % MOD;
+		}
+		return result;
+	};
+
+	return b.reduceRight((result, n) => {
+		a %= MOD;
+		const powNum = result * pow(a, n) % MOD;
+
+		a = pow(a, 10);
+		return powNum;
+	}, 1);
+};
