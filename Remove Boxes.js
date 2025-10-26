b48e74ba364e363aaf80680e6e750e75d5651a1e
ANANDU VIJAYAN V M <ananduvijayan8@gmail.com>
2025-10-26 11:44:13 +0530

    Remove Boxes

--- Remove Boxes.js
+++ Remove Boxes.js
@@ -1,44 +1,44 @@
-546. Remove Boxes
+// 546. Remove Boxes
 
-Hard
+// Hard
 
-Topics
+// Topics
 
-Companies
+// Companies
 
-You are given several boxes with different colors represented by different positive numbers.
+// You are given several boxes with different colors represented by different positive numbers.
 
-You may experience several rounds to remove boxes until there is no box left. Each time you can choose some continuous boxes with the same color (i.e., composed of k boxes, k >= 1), remove them and get k * k points.
+// You may experience several rounds to remove boxes until there is no box left. Each time you can choose some continuous boxes with the same color (i.e., composed of k boxes, k >= 1), remove them and get k * k points.
 
-Return the maximum points you can get.
+// Return the maximum points you can get.
 
-Example 1:
+// Example 1:
 
-Input: boxes = [1,3,2,2,2,3,4,3,1]
+// Input: boxes = [1,3,2,2,2,3,4,3,1]
 
-Output: 23
+// Output: 23
 
-Explanation:
+// Explanation:
 
-[1, 3, 2, 2, 2, 3, 4, 3, 1]
+// [1, 3, 2, 2, 2, 3, 4, 3, 1]
 
-Example 2:
+// Example 2:
 
-[1, 3, 3, 4, 3, 1] (3*3=9 points)
+// [1, 3, 3, 4, 3, 1] (3*3=9 points)
 
-[1, 3, 3, 3, 1] (1*1=1 points)
+// [1, 3, 3, 3, 1] (1*1=1 points)
 
-[1, 1] (3*3=9 points)
+// [1, 1] (3*3=9 points)
 
-[] (2*2=4 points)
+// [] (2*2=4 points)
 
-Input: boxes = [1,1,1]
+// Input: boxes = [1,1,1]
 
-Output: 9
+// Output: 9
 
-Example 3:
+// Example 3:
 
-Input: boxes
+// Input: boxes
 
 
 /**
