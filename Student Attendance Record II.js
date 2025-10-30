e41a386c9520e881864c4ef867aa40f755ff0c0c
ANANDU VIJAYAN V M <ananduvijayan8@gmail.com>
2025-10-30 09:33:42 +0530

    Brick Wall

--- Student Attendance Record II.js
+++ Student Attendance Record II.js
@@ -1,26 +1,26 @@
-552. Student Attendance Record II
+// 552. Student Attendance Record II
 
-Topics
+// Topics
 
-Companies
+// Companies
 
-Hard
+// Hard
 
-An attendance record for a student can be represented as a string where each character signifies whether the student was absent, late, or present on that day. The record only contains the following three characters:
+// An attendance record for a student can be represented as a string where each character signifies whether the student was absent, late, or present on that day. The record only contains the following three characters:
 
-'A': Absent.
+// 'A': Absent.
 
-'L': Late.
+// 'L': Late.
 
-'p': Present.
+// 'p': Present.
 
-Any student is eligible for an attendance award if they meet both of the following criteria:
+// Any student is eligible for an attendance award if they meet both of the following criteria:
 
-The student was absent ('A') for strictly fewer than 2 days total.
+// The student was absent ('A') for strictly fewer than 2 days total.
 
-The student was never late ('L') for 3 or more consecutive days.
+// The student was never late ('L') for 3 or more consecutive days.
 
-Given an integer n, return the number of possible attendance records of length n that make a student eligible for an attendance award. The answer may be very large, so return it modulo 10º + 7.
+// Given an integer n, return the number of possible attendance records of length n that make a student eligible for an attendance award. The answer may be very large, so return it modulo 10º + 7.
 
 
 
